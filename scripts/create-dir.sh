d2a94a47b183cf87d6f1038aa9dbeb26943afa90
ZeiZel <valera2003lvov@yandex.ru>
2025-05-09 22:32:17 +0300

    fix: fixed compose drop

--- scripts/create-dir.sh
+++ scripts/create-dir.sh
@@ -1,2 +1,7 @@
-mkdir -p -m 750 ../volumes/{youtrack/{data,logs,backups,conf},gitlab/{config,logs,data,var/opt/gitlab,var/log/gitlab,etc/gitlab-runner,var/run/docker.sock},notesnook/data,revolt/data,authentik/{db,media},caddy/{data,config}}
-chown -R 13001:13001 ../volumes/youtrack
+mkdir -p -m 750 \
+  ../volumes/{youtrack/{data,logs,backups,conf}, \
+  gitlab/{config,logs,data,var/opt/gitlab,var/log/gitlab,etc/gitlab-runner,var/run/docker.sock}, \
+  notesnook/data,revolt/data,authentik/{db,media}, \
+  caddy/{data,config}}
+sudo chown -R 13001:13001 ../volumes/youtrack
+sudo chown -R 65534:65534 ../volumes/prometheus
